5f7cd5b98322c6b6d06c085f76aee1eae2f789c2
zachary-algaze <zalgaze@gmail.com>
2024-10-22 08:27:20 -0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 import './app.css'
 
+var name = prompt("What is your name? Please type your name in: ");
+var header_output = <h1>Hello {name}</h1>
+
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit
+      {header_output}
+      React ⚛️ + Vite ⚡ + Github
     </main>
   )
 }
