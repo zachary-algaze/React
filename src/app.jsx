7c7b325e3755243aeab419d0ef0305a076cc83f7
zachary-algaze <zalgaze@gmail.com>
2024-10-22 11:37:22 -0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import './app.css'
 
-var name = prompt("What is your name? Please type your name in: ");
-var header_output = <h1>Hello {name}</h1>
+var name = prompt("What is your name? Please type your name in the box below: ");
+var header_output = <h1>Hello {name}</h1>;
+var currentYear = new Date().getFullYear();
+var footer_output = <h5>Copyright {currentYear} </h5>;
 
 export default function App() {
   return (
     <main>
       {header_output}
       React ⚛️ + Vite ⚡ + Github
+      {footer_output}
     </main>
   )
 }
